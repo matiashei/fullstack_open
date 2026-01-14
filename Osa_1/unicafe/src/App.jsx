eb36b9d06c0db0a7595526b720f841e9a823332e
Matias Heino <matias.heino@hotmail.com>
2026-01-14 19:39:55 +0200

    Add Osa_1 & Osa_2

---
+++ Osa_1/unicafe/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={good + neutral + bad} />
+        <StatisticLine text='average' value={(good - bad) / (good + neutral + bad)} />
+        <StatisticLine text='positive' value={`${good / (good + neutral + bad) * 100} %`} />
+      </tbody>
+    </table>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    const updateGood = good + 1
+    setGood(updateGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updateNeutral = neutral + 1
+    setNeutral(updateNeutral)
+  }
+
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    setBad(updateBad)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
---
+++ Osa_2/kurssitiedot/src/App.jsx
@@ -0,0 +1,70 @@
+const totalAmount = (parts) => parts.reduce((sum, part) => sum + part.exercises, 0);
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => <div key={part.id}>{part.name + ' ' + part.exercises}</div>)}
+    </>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+    <div>
+      <Course key={course.id} course={course} />
+    </div>
+    <div>
+      <b>total of {totalAmount(course.parts)} exercises</b>
+    </div>
+    </>
+  )
+}
+
+export default App
