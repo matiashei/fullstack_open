21abc69183e1acf4c58d99b428264d3c709d223a
Matias Heino <matias.heino@hotmail.com>
2026-01-21 13:25:19 +0200

    Add 3.3-3.4

--- Osa_3/puhelinluettelo_backend/index.js
+++ Osa_3/puhelinluettelo_backend/index.js
@@ -28,11 +28,29 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
     response.send(`<div>Phonebook has info for ${persons.length} people<br>${new Date()}</div>`)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
