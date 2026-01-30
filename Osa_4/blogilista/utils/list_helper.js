5639e2773a10abeca76e9b729b7c225ed41b8145
Matias Heino <matias.heino@hotmail.com>
2026-01-30 16:22:32 +0200

    Add 4.3-4.5

--- Osa_4/blogilista/utils/list_helper.js
+++ Osa_4/blogilista/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return 0
+  }
+  const maxBlog = blogs.reduce((max, current) => {
+    return current.likes > max.likes ? current : max
+  })
+  return maxBlog.likes
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
