ee85d7e4caf71db377022760be06543e85b8528b
Matias Heino <matias.heino@hotmail.com>
2026-02-10 17:06:56 +0200

    Add 5.14-5.15

--- Osa_5/bloglist-frontend/src/components/Blog.test.jsx
+++ Osa_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { expect } from 'vitest'
 
 beforeEach(() => {
   window.localStorage.setItem(
@@ -23,6 +24,59 @@ test('renders content', () => {
 
   render(<Blog blog={blog} />)
 
-  const element = screen.getByText('Test blog')
+  expect(screen.getByText('Test blog')).toBeInTheDocument()
 })
 
+test('clicking view button shows url and likes', async () => {
+  const blog = {
+    title: 'Test blog',
+    author: 'Tim Tester',
+    url: 'http://example.com/test-blog',
+    likes: 10,
+    user: {
+      name: 'Test User',
+      username: 'testuser',
+    },
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog blog={blog} handleLike={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText('http://example.com/test-blog', { exact: false })).toBeInTheDocument()
+  expect(screen.getByText('likes 10', { exact: false })).toBeInTheDocument()
+})
+
+test('clicking like button increases likes', async () => {
+  const blog = {
+    title: 'Test blog',
+    author: 'Tim Tester',
+    url: 'http://example.com/test-blog',
+    likes: 10,
+    user: {
+      name: 'Test User',
+      username: 'testuser',
+    },
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog blog={blog} handleLike={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler).toHaveBeenCalledTimes(2)
+})
