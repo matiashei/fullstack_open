7be2dd2be74d5f13186539c9f57852f2f1cbdee7
Matias Heino <matias.heino@hotmail.com>
2026-02-17 13:15:34 +0200

    Add 6.14

---
+++ Osa_6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,14 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch anecdotes')
+  }
+
+  const date = await response.json()
+  return date
+}
+
+export default { getAll }
