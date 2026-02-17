a79f193b24ae82cea4ce035a1732d27e904ff199
Matias Heino <matias.heino@hotmail.com>
2026-02-17 15:07:54 +0200

    Add 6.16

--- Osa_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Osa_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -35,6 +27,15 @@ const anecdoteSlice = createSlice({
   }
 })
 
+const { setAnecdotes } = anecdoteSlice.actions
 
-export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+    }
+  }
+
+export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+
 export default anecdoteSlice.reducer
