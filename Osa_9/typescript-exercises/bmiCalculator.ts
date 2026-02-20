5b6690ff100a15fcf0145ed78f22c68560649bf9
Matias Heino <matias.heino@hotmail.com>
2026-02-20 10:55:44 +0200

    Add 9.3

--- Osa_9/typescript-exercises/bmiCalculator.ts
+++ Osa_9/typescript-exercises/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+export const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Too few arguments!');
+  if (args.length > 4) throw new Error('Too many arguments!')
+
+  if (!isNaN(Number(args[2])) && (!isNaN(Number(args[3])))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
 const calculateBmi = (height: number, weight: number) => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters ** 2);
@@ -11,4 +30,13 @@ const calculateBmi = (height: number, weight: number) => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- Osa_9/typescript-exercises/exerciseCalculator.ts
+++ Osa_9/typescript-exercises/exerciseCalculator.ts
@@ -8,6 +8,22 @@ interface Result {
   average: number,
 }
 
+export const parseArguments = (args: string[]): { dailyHours: number[], target: number } => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const target = Number(args[2]);
+  const dailyHours = args.slice(3).map(n => Number(n));
+
+  if (isNaN(target) || dailyHours.some(n => isNaN(n))) {
+    throw new Error('Provided values were not numbers!');
+  }
+
+  return {
+    dailyHours,
+    target
+  }
+}
+
 const calculateExercises = (dailyHours: number[], target: number): Result => {
   const periodLength = dailyHours.length;
   const trainingDays = dailyHours.reduce((x, n) => {
@@ -48,4 +64,14 @@ const calculateExercises = (dailyHours: number[], target: number): Result => {
     average
   }
 }
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+  const { dailyHours, target } = parseArguments(process.argv);
+  console.log(calculateExercises(dailyHours, target));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
