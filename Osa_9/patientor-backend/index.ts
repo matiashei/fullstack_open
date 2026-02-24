756628ec0b88eb16b9a7194e1573b22712b953f4
Matias Heino <matias.heino@hotmail.com>
2026-02-24 12:53:47 +0200

    Add 9.9

--- Osa_9/patientor-backend/index.ts
+++ Osa_9/patientor-backend/index.ts
@@ -1,12 +1,19 @@
+import cors from 'cors';
 import express from 'express';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, res) => {
+  res.send('patients');
 });
 
 app.listen(PORT, () => {
